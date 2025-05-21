ea5da0551771f04f51c7fcf8f9b0cca682e82cbd
archanakumari-dev <archukumari5455@gmail.com>
2025-05-21 18:58:10 +0530

    basename added

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-    <BrowserRouter>
+    <BrowserRouter basename='/hotel-booking'>
        <App />
     </BrowserRouter>,
   </ClerkProvider>
